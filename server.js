2fb1861788aeed01ed17bb4485d269afc30c03dc
Gustav Henning <nightmunnas@gmail.com>
2016-04-09 19:24:35 +0200

    hello app

---
+++ server.js
@@ -0,0 +1,17 @@
+"use strict"; 
+
+	/* dependencies */
+    var express  = require('express');
+    var app      = express();                               
+    var morgan = require('morgan');             // log requests to the console (express4)
+    var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+
+    /* init packages */
+    app.use(express.static(__dirname + '/public'));
+    app.use(morgan('dev'));    /* log everything to console */
+	app.use(bodyParser.urlencoded({'extended' : 'false'}));
+    app.use(bodyParser.json());
+	
+    /* server start */
+	app.listen(8080);
+    console.log("App listening on port 8080");
