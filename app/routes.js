5b3624eb6019e4dda9c37b69a7396d0ae9c5806c
jpttsn <jopt@me.com>
2016-04-22 11:47:35 +0200

    Connect routes to app. Fix minimum version req. of expressjs

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,8 @@
 "use strict";
 
 module.exports = function(app){
-    app.get('*', function (req, res) {
-        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-    });
+	app.get('/twitter', function (req, res) {
+		console.log("Got it");
+		res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+	});
 }
--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 "use strict"; 
 
-	/* dependencies */
-    var express  = require('express');
-    var app      = express();                               
-    var morgan = require('morgan');             // log requests to the console (express4)
-    var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+/* dependencies */
+var express  = require('express');
+var app      = express();                      
+var morgan = require('morgan');             // log requests to the console (express4)
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 
-    /* init packages */
-    app.use(express.static(__dirname + '/public'));
-    app.use(morgan('dev'));    /* log everything to console */
-	app.use(bodyParser.urlencoded({'extended' : 'false'}));
-    app.use(bodyParser.json());
-	
-    /* server start */
-	app.listen(8080);
-    console.log("App listening on port 8080");
+/* init packages */
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));    /* log everything to console */
+app.use(bodyParser.urlencoded({'extended' : 'false'}));
+app.use(bodyParser.json());
+
+// require(__dirname+'/app/routes.js')(app)
+app.get('/twitter', function (req, res) {
+    console.log("Got it");
+    res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
+/* server start */
+app.listen(8080);
+console.log("App listening on port 8080");
