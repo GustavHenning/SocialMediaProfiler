2e9d3cb52f5b29ca7b5a6e50af6b381576ebfa9b
jpttsn <jopt@me.com>
2016-04-22 12:41:55 +0200

    Twitter timeline with more data and instagram module

--- app/routes.js
+++ app/routes.js
@@ -10,23 +10,29 @@ var twitterClient = new Twitter({
 	access_token_secret: 'yjTIxoXIoemi6tA2kBR1drQIqAXCBquTel1nUcrmMUeox'
 });
 
+var instagramClient = require('instagram').createClient('<client_id>', '<client_secret>');
+
 module.exports = function(app){
 	app.get('/twitter/:handle', function (req, res) {
 		var params = {screen_name: req.params.handle};
 		twitterClient.get('statuses/user_timeline', params, function(error, tweets, response) {
 			if (!error) {
-				res.setHeader("Content-Type", "text/html");
+				res.setHeader("Content-Type", "text/html;Charset=utf-8");
 				res.writeHead(200);
 				res.write('<h1>'+req.params.handle+'</h1>')
+				res.write('<h2>'+tweets[0].user.description+'</h2>')
+				console.log(tweets[0]);
+				res.write('<ul>');
 				tweets.forEach(function(tweet, i, tweets) {
-					console.log(tweet.text);
-					res.write(tweet.text);
-					res.write('<br>');
+					res.write('<li>'+tweet.text+'</li>');
 				});
-				console.log("Done");
+				res.write('</ul>');
+				console.log("Done Twitter");
+				instagramClient.users.search(req.params.handle, function (users, error) {
+					console.log(users)
+				});
 				res.end();
 			}
 		});
-		// res.sendFile(path.resolve(__dirname + '/../public/index.html')) // load the single view file (angular will handle the page changes on the front-end)
 	});
 }
