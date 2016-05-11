eb067756e054dfe7166a4021c55aa01122b6e350
jpttsn <jopt@me.com>
2016-05-11 12:08:59 +0200

    Rewrite routes.js to use promises via Q

--- app/routes.js
+++ app/routes.js
@@ -3,69 +3,101 @@
 var path = require("path");
 var jsdom = require('jsdom');
 var async = require('async');
+var Q = require('q');
 
 /* configs */
 var twitterCfg = require('../config/twitter.json');
 var instagramCfg = require('../config/instagram.json');
 
 /* clients */
-var instagram = require('nodestagram').createClient(instagramCfg.client_id, instagramCfg.client_secret);
+var instagramClient = require('nodestagram').createClient(instagramCfg.client_id, instagramCfg.client_secret);
 var twitterClient = require('twitter')(twitterCfg);
 
 var profiler = require('../lib/profiler');
 
 module.exports = function(app) {
-  app.post('/searches', function(req, res) {
-    res.setHeader("Content-Type", "text/html;Charset=utf-8");
-    res.writeHead(200);
-    /* Given a name, do a generic search for data.
-     * TODO: Make async better, now we are chaining like plebs (Maybe use promises?) */
-    var calls = [];
-    /* TWITTER */
-    twitterClient.get('users/search', {
-      q: req.body.fullName
-    }, function(err, users, response) {
-      if (!err) {
-        users.forEach(function(user, i, users) {
-          calls.push(function(callback) {
-            profiler.putData(user.screen_name, "twitter", user);
-            res.write("<li> " + user.screen_name + ": twitter </li>");
-            callback();
-          });
-        });
-      } else {
-        console.error("Error searching for usernames on twitter: " + err);
-      }
-      /* INSTAGRAM */
-      instagram.users.search(req.body.fullName, function(users, err) {
-        if (!err) {
-          users.forEach(function(user, i, users) {
-            calls.push(function(callback) {
-              console.log(user);
-              profiler.putData(user.full_name.toString(), "instagram", user);
-              res.write("<li> " + user.full_name.toString() + ": instagram </li>");
-              callback();
-            });
-          });
-        } else {
-          console.error("Error searching for usernames on instagram: " + err);
-        }
-        /* TODO: Add general facebook, linkedin search */
-        /* ASYNC */
-        async.parallel(calls, function(err, result) {
-          if (err)
-            return console.log(err);
-          profiler.setRelevance();
-          res.write('<h1>Done!</h1>');
-          res.end();
-        });
-      });
-    });
-    /* TODO: Add additional searches based on usernames(?) */
-  });
 
+	app.get("/", function(req, res) {
+		res.sendFile(path.resolve(__dirname + '/../public/index.html')) // load the single view file (angular will handle the page changes on the front-end)
+	});
 
-  app.get("/", function(req, res) {
-    res.sendFile(path.resolve(__dirname + '/../public/index.html')) // load the single view file (angular will handle the page changes on the front-end)
-  });
+	app.post('/searches', function(req, res) {
+		
+		/* Helpers that use our res */
+
+		var sendHeaders = function() {
+			res.setHeader("Content-Type", "text/html;Charset=utf-8");
+			res.writeHead(200);			
+		}
+		var printHit = function (s) {
+			res.write("<li>"+s+"</li>");
+		}
+
+		/* Given a name, do a generic search for data. */
+
+		Q.fcall(sendHeaders)
+		.then(function() {
+			return promiseTwitterSearch(req.body.fullName);
+		})
+		.then(function (hits) {
+			for (var i = 0; i < hits.length; i++) {
+				var user = hits[i]
+				printHit(user.screen_name + ": twitter");
+				profiler.putData(user.screen_name, "twitter", user);
+			}
+		}, function(err) {
+			console.error("twitterSearch failed:"+err)
+		})
+		.then(function() {
+			return promiseInstagramSearch('justin bieber');
+		})
+		.then(function(hits) {
+			for (var i = 0; i < hits.length; i++) {
+				printHit(hits[i].screen_name + ": instagram");
+				profiler.putData(user.full_name.toString(), "instagram", user);
+			}
+		}, function(err) {
+			console.error("instagramSearch failed:"+err)
+		})
+		.then(function() {
+			profiler.setRelevance();
+			res.write('<h1>Done!</h1>');
+    		res.end();
+		});
+
+		/* TODO: Add additional searches based on usernames(?) */
+		
+	});
+}
+
+var promiseTwitterSearch = function(name) {
+	var deferred = Q.defer();
+	twitterClient.get('users/search', {q: name}, function(err, users, response) {
+		var hits = []
+		if (!err) {
+			users.forEach(function(user, i, users) {
+				hits.push(user);
+			});
+			deferred.resolve(hits) /* This is a little like a callback */
+		} else {
+			deferred.reject(err);
+		}
+	});
+	return deferred.promise;
+}
+
+var promiseInstagramSearch = function(name) {
+	var deferred = Q.defer();
+	instagramClient.users.search(name, function(users, err) {
+		var hits = []
+		if (!err) {
+			users.forEach(function(user, i, users) {
+				hits.push(user)
+			});
+			deferred.resolve(hits);
+		} else {
+			deferred.reject(err);
+		}
+	});
+	return deferred.promise;
 }
