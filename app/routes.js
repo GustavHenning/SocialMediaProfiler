131784c3018e5b3f62416c83e8f0e0018ef1df75
Gustav Henning <nightmunnas@gmail.com>
2016-05-16 16:37:54 +0200

    final frontend

--- app/routes.js
+++ app/routes.js
@@ -27,138 +27,181 @@ mediaPic["linkedin"] = "http://imgur.com/xwsrA5H.png";
 
 module.exports = function(app) {
 
-	app.get("/", function(req, res) {
-		res.sendFile(path.resolve(__dirname + '/../public/index.html')) // load the single view file (angular will handle the page changes on the front-end)
-	});
+  app.get("/", function(req, res) {
+    res.sendFile(path.resolve(__dirname + '/../public/index.html')) // load the single view file (angular will handle the page changes on the front-end)
+  });
 
-	app.post('/searches', function(req, res) {
+  app.post('/searches', function(req, res) {
 
-		/* Helpers that use our res */
+    /* Helpers that use our res */
 
-		var sendHeaders = function() {
-			res.setHeader("Content-Type", "text/html;Charset=utf-8");
-			res.writeHead(200);
-		}
-		var printHit = function (s) {
-			res.write("<li>"+s+"</li>");
-		}
+    var sendHeaders = function() {
+      res.setHeader("Content-Type", "text/html;Charset=utf-8");
+      res.writeHead(200);
+    }
+    var printHit = function(s) {
+      res.write("<li>" + s + "</li>");
+    }
 
-		/* Given a name, do a generic search for data. */
+    /* Given a name, do a generic search for data. */
 
-		Q.fcall(sendHeaders)
-		.then(function() {
-			return promiseTwitterSearch(req.body.fullName);
-		})
-		.then(function (hits) {
-			for (var i = 0; i < hits.length; i++) {
-				var user = hits[i];
-				printHit(user.screen_name + ": twitter");
-				profiler.putData(user.screen_name, "twitter", user);
-			}
-		}, function(err) {
-			console.error("twitterSearch failed: " + err);
-		})
-		.then(function() {
-			return promiseInstagramSearch(req.body.fullName);
-		})
-		.then(function(hits) {
-			for (var i = 0; i < hits.length; i++) {
-				var user = hits[i];
-				printHit(user.full_name + ": instagram");
-				profiler.putData(user.full_name, "instagram", user);
-			}
-		}, function(err) {
-			console.error("instagramSearch failed: "+ err);
-		}).then(function(){
-			/* TEST PROFILES */
-			if(TESTING){
-				console.log("Testing enabled: adding dummy profiles");
-				for(var testProfile in fbTests){
-					printHit("[TestProfile] " + fbTests[testProfile].name + ": facebook");
-					profiler.putData(fbTests[testProfile].name, "facebook", fbTests[testProfile]);
-				}
-				for(var testProfile in instaTests){
-					printHit("[TestProfile] " + instaTests[testProfile].name + ": instagram");
-					profiler.putData(instaTests[testProfile].name, "instagram", instaTests[testProfile]);
-				}
-			}
-		})
-		.then(function() {
-			profiler.setRelevance();
-			injectResults(res, profiler.combineProfiles());
-			res.write('<h1>Done!</h1>');
-    		res.end();
-		});
+    Q.fcall(sendHeaders)
+      .then(function() {
+        return promiseTwitterSearch(req.body.fullName);
+      })
+      .then(function(hits) {
+        for (var i = 0; i < hits.length; i++) {
+          var user = hits[i];
+          //printHit(user.screen_name + ": twitter");
+          profiler.putData(user.screen_name, "twitter", user);
+        }
+      }, function(err) {
+        console.error("twitterSearch failed: " + err);
+      })
+      .then(function() {
+        return promiseInstagramSearch(req.body.fullName);
+      })
+      .then(function(hits) {
+        for (var i = 0; i < hits.length; i++) {
+          var user = hits[i];
+          //printHit(user.full_name + ": instagram");
+          profiler.putData(user.full_name, "instagram", user);
+        }
+      }, function(err) {
+        console.error("instagramSearch failed: " + err);
+      }).then(function() {
+        /* TEST PROFILES */
+        if (TESTING) {
+          console.log("Testing enabled: adding dummy profiles");
+          for (var testProfile in fbTests) {
+            printHit("[TestProfile] " + fbTests[testProfile].name + ": facebook");
+            profiler.putData(fbTests[testProfile].name, "facebook", fbTests[testProfile]);
+          }
+          for (var testProfile in instaTests) {
+            printHit("[TestProfile] " + instaTests[testProfile].name + ": instagram");
+            profiler.putData(instaTests[testProfile].name, "instagram", instaTests[testProfile]);
+          }
+        }
+      })
+      .then(function() {
+        profiler.setRelevance();
+        injectResults(res, profiler.combineProfiles());
+        res.write('<h1>Done!</h1>');
+        res.end();
+      });
 
-		/* TODO: Add additional searches based on usernames(?) */
+    /* TODO: Add additional searches based on usernames(?) */
 
-	});
+  });
 }
 
 var promiseTwitterSearch = function(name) {
-	var deferred = Q.defer();
-	twitterClient.get('users/search', {q: name}, function(err, users, response) {
-		var hits = []
-		if (!err) {
-			users.forEach(function(user, i, users) {
-				hits.push(user);
-			});
-			deferred.resolve(hits) /* This is a little like a callback */
-		} else {
-			deferred.reject(err);
-		}
-	});
-	return deferred.promise;
+  var deferred = Q.defer();
+  twitterClient.get('users/search', {
+    q: name
+  }, function(err, users, response) {
+    var hits = []
+    if (!err) {
+      users.forEach(function(user, i, users) {
+        hits.push(user);
+      });
+      deferred.resolve(hits) /* This is a little like a callback */
+    } else {
+      deferred.reject(err);
+    }
+  });
+  return deferred.promise;
 };
 
 var promiseInstagramSearch = function(name) {
-	var deferred = Q.defer();
-	instagramClient.users.search(name, function(users, err) {
-		console.log(users);
-		var hits = [];
-		if (!err) {
-			users.forEach(function(user, i, users) {
-				hits.push(user);
-			});
-			deferred.resolve(hits);
-		} else {
-			console.log(err);
-			deferred.reject(err);
-		}
-	});
-	return deferred.promise;
+  var deferred = Q.defer();
+  instagramClient.users.search(name, function(users, err) {
+    console.log(users);
+    var hits = [];
+    if (!err) {
+      users.forEach(function(user, i, users) {
+        hits.push(user);
+      });
+      deferred.resolve(hits);
+    } else {
+      console.log(err);
+      deferred.reject(err);
+    }
+  });
+  return deferred.promise;
 };
 /* Injects the JSON response from the profiler into the DOM */
 var injectResults = function(res, profiles) {
-	res.write("<div class='row'>");
-	for(var prof in profiles){
-		res.write("<p> User: </p>");
-		var media = Object.keys(profiles[prof]);
-		res.write("<div class='col-md-4'>"); /* 4 needs to be replaced by number of social medias in json */
-		/* Social media pictures */
-		for(var n in media){
-			if(mediaPic[media[n]]){
-				res.write("<img src='" + mediaPic[media[n]] + "'/>");
-			}
-		}
-		/* Actual info */
-		injectJSON(res, profiles[prof]);
-		res.write("</div>");
-	}
-	res.write("</div>");
+  res.write("<div class='row' style='max-width:600px; margin:auto;'>");
+  for (var prof in profiles) {
+    var media = Object.keys(profiles[prof]);
+    res.write("<div class='col-md-6'>"); /* 4 needs to be replaced by number of social medias in json */
+    /* Social media pictures */
+    for (var n in media) {
+      if (mediaPic[media[n]]) {
+        res.write("<img src='" + mediaPic[media[n]] + "' style='max-width:50px;max-height:50px;' />");
+      }
+    }
+    /* Actual info */
+    injectJSON(res, profiles[prof]);
+    res.write("</div>");
+  }
+  res.write("</div>");
 };
 
-var injectJSON = function(res, json){
-	for(var k in json){
-		for(var key in json[k]){
-		var s = json[k][key];
-		console.log(s);
-		/* images */
-		if(s.indexOf("http") == 0 && (s.indexOf(".jpg") > -1 || s.indexOf(".jpeg") > -1 || s.indexOf(".png") > -1 || s.indexOf(".gif") > -1)){
-			res.write("<img src='" + s + "'/>");
-		} else { /* other fields */
-			res.write("<li>" + s + "</li>");
-		}
-	}
-	}
+var injectJSON = function(res, json) {
+  var seen = [];
+  /* Not combined result */
+  if (Object.keys(json).length > 5) {
+    var keys = Object.keys(json);
+		injectImage(res, mediaPic[json["apiType"]]);
+    for (var ke in keys) {
+      var s = json[keys[ke]];
+      /* images */
+      if (isImage(s) && seen.indexOf(s.replace("https", "http")) == -1) {
+        injectImage(res, s);
+        seen.push(s.replace("https", "http"));
+      }
+    }
+    for (var ke in keys) {
+      var s = json[keys[ke]];
+      if (!isImage(s)) {
+        injectString(res, keys[ke], s);
+      }
+    }
+  } else { /* combined */
+    for (var k in json) {
+      var keys = Object.keys(json[k]);
+      for (var ke in keys) {
+        var s = json[k][keys[ke]];
+        if (isImage(s) && seen.indexOf(s.replace("https", "http")) == -1) {
+          injectImage(res, s);
+          seen.push(s.replace("https", "http"));
+        }
+      }
+    }
+    for (var k in json) {
+      var keys = Object.keys(json[k]);
+      for (var ke in keys) {
+        var s = json[k][keys[ke]];
+        if (!isImage(s)) {
+          injectString(res, keys[ke], s);
+        }
+      }
+    }
+  }
+};
+
+var isImage = function(s) {
+  return s.indexOf("http") == 0 && (s.indexOf(".jpg") > -1 || s.indexOf(".jpeg") > -1 || s.indexOf(".png") > -1 || s.indexOf(".gif") > -1);
+};
+
+var injectImage = function(res, i) {
+  res.write("<img src='" + i + "' style='max-width:200px;max-height:200px;'/>");
+};
+var injectString = function(res, key, s) {
+  if (s.length > 1 && key !== "apiType" && key.indexOf("color") === -1 && key.indexOf("id") === -1) { /* other fields */
+    res.write("\n <p><b>" + key.replace("_", " ").replace("_", " ") + ": </b>");
+		res.write(s + "</p>");
+  }
 };
